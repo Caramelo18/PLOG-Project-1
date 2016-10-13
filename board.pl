b8de6c08169ea1e6b802cf975edb411b9e253650
rodavoce <up201404178@fe.up.pt>
2016-10-14 00:44:46 +0100

    sketch board

--- board.pl
+++ board.pl
@@ -9,33 +9,47 @@ board([[s,s,s,s,s,s],
 
 
 display_board([]):-nl.
-display_board([L1|Ls]):- display_line(L1), nl, display_board(Ls).
+display_board([L1|Ls]):- display_line_p1(L1), nl,
+                         display_line_p2(L1), nl,
+                         display_line_p3(L1),nl,
+                         display_board(Ls).
 
-display_line([]):-nl.
-display_line([E]):- translate(E),nl, display_row_separator(E).
-display_line([E|Es]):- translate(E), write('|'), display_line(Es).
+display_line_p1([]).
+display_line_p1(E):- display_tile_p1(E),nl.
+display_line_p1([E|Es]):- display_tile_p1(E),
+                          write('|'),
+                          display_line_p1(Es).
+
+display_line_p2([]).
+display_line_p2(E):- display_tile_p2(E,g),nl.
+display_line_p2([E|Es]):- display_tile_p2(E,g),
+                          write('|'),
+                          display_line_p2(Es).
+
+display_line_p3([]).
+display_line_p3(E):- display_tile_p3(E),nl.
+display_line_p3([E|Es]):- display_tile_p3(E),
+                          write('|'),
+                          display_line_p3(Es).
+
+display_tile_p1(E):- E == p1 -> draw_tile1_MV;
+                     E == p2 -> draw_tile2_1LR;
+                     E == s -> draw_empty.
+
+display_tile_p2(E,R):- E == p1 -> draw_empty(R);
+                       E == p2 -> draw_tile2_3LR(R);
+                       E == s  -> draw_empty(R).
+
+display_tile_p3(E):- E == p1 -> draw_empty;
+                     E == s -> draw_empty.
 
 
-display_row_separator(E):- write('___|___|___|___|___|___').
+
+display_row_separator():- write('___|___|___|___|___|___').
 display_first_line(E):-    write('_______________________'), nl.
 
-%casa do tabuleiro vazia
-translate(s):- write('   ').
-%cor do jogador
-translate(r):- write('R').
-translate(g):- write('G').
 
-%tipos de peças
-translate(p1):- write('1').
-translate(p2):- write('2').
-translate(p3):- write('3').
-translate(p4):- write('4').
-translate(p8):- write('8').
-translate(pX):- write('X').
-
-
-
-/*Pool de Soldados*/
+/*Pool dee Soldados*/
 tiles_pool( [[p1,p1,p1,p1,p1,p1,p1],
              [p2,p2,p2,p2,p2,p2,p2],
              [p3,p3,p3,p3,p3,p3,p3],
@@ -73,3 +87,23 @@ getSolier(S). % retirar  um Soldado aleatorio á pool.
 
 /*Game end */
 game_end(D). % todos os quadrados atribuidos a jogadores  e sem pecas em campo.
+
+
+draw_tile8_1:- write(''\' | /').
+draw_tile8_2(R):- write('--'), write(R), write('--').
+draw_tile8_3:- write('/ | '\'').
+
+draw_tile1_MV:- write(' | ').
+draw_tile1_MHL(R):-write('--'),write(R),write('  ').
+draw_tile1_MHR(R):-write('  '),write(R),write('--').
+
+draw_empty:- write('     ').
+draw_empty(R):-write('  '),write(R),write('  ').
+
+draw_tile2_1LR:- write(''\'   ').
+draw_tile2_1RL:- write('    /').
+draw_tile2_3LR:- write('    '\'').
+draw_tile2_3RL:- write('/    ').
+
+
+game(X):-board(X),display_board(X).
