feeb189636364203e2c703705db6b3e94bbd19d9
rodavoce <up201404178@fe.up.pt>
2016-11-07 23:35:26 +0000

    interface func

---
+++ interface.pl
@@ -0,0 +1,55 @@
+:- use_module(library(system)).
+:- use_module(library(random)).
+
+getInt(Input):-
+                get_code(Tinput),
+                Input is Tinput - 48.
+
+getChar(Input):-get_char(Input),
+                get_char(_).
+
+getCode(Input):- get_code(TempInput),
+                 %get_code(_),
+              	 Input is TempInput.
+
+inputCoords(Row,Col):-  %read row
+                        getCode(RRow),
+
+                        %read Col
+
+                        getInt(RCol),
+
+                        get_code(_),
+                        Row is RRow - 65,
+                        Col is RCol - 1. % A = 41.
+
+getNewTileCoord(Row,Col):-
+    write('Inser coordinates to place tile: [ Row, Col ]'),
+    inputCoords(Row,Col).
+
+
+
+displayPlayerHand(L1,Pname):-  %draw all pieces in player hand
+                          write('Player '),
+                          write(Pname),
+                          write(' hand: '),
+                          nl,
+                          write('  -------------------'),
+                          nl,
+                          write('  |'),
+                          display_line_p1(L1),
+                          write('  |'),
+                          display_line_p2(L1),
+                          write('  |'),
+                          display_line_p3(L1),
+                          write('  -------------------').
+
+
+%testes
+
+playerhand([tile(a,t2,u),     tile(a,t2,l), tile(a,t2,r)]).
+thand:-playerhand(X),displayPlayerHand(X,'PENIS').
+tinput:- getNewTileCoord(X,Y),write(X), nl, write(Y),nl.
+
+
+%RANDOM
